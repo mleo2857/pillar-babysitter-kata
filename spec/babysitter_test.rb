20e39c1f91079662f73584485e261c8737106e15
Michael Leo <mleo2857@gmail.com>
2018-10-28 12:29:09 -0400

    RED - added test to check for instance of Babysitter

--- spec/babysitter_test.rb
+++ spec/babysitter_test.rb
@@ -4,4 +4,9 @@ require 'minitest/spec'
 require '../lib/babysitter'
 
 describe Babysitter do
+  subject {Babysitter.new}
+
+  it 'must be an must be an instance of babysitter' do
+    subject.must_be_instance_of Babysitter
+  end
 end
