b0f51c70f386e46781c6b52f9038910a207aae0b
Michael Leo <mleo2857@gmail.com>
2018-10-28 12:33:18 -0400

    RED - added test to check that Babysitter has a 'name' attribute

--- spec/babysitter_test.rb
+++ spec/babysitter_test.rb
@@ -9,4 +9,8 @@ describe Babysitter do
   it 'must be an must be an instance of babysitter' do
     subject.must_be_instance_of Babysitter
   end
+
+  it 'must have a name' do
+    subject.must_respond_to 'name'
+  end
 end
